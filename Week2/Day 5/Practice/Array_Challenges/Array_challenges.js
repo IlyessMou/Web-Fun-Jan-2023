584e7f02c4fc755ebc9e4ac3c128a72fd24922d8
unknown <ilyessmoulazem1@gmail.com>
2023-07-07 17:07:40 +0200

    Array_Challenges

--- Week2/Day 5/Practice/Array_Challenges/Array_challenges.js
+++ Week2/Day 5/Practice/Array_Challenges/Array_challenges.js
@@ -58,7 +58,7 @@ function fibonacciArray(n) {
     // the [0, 1] are the starting values of the array to calculate the rest from
     var fibArr = [0, 1];
     // your code here
-    for (var i=2;i<10;i++){
+    for (var i=2;i<n;i++){
         fibArr[i]=fibArr[i-1]+fibArr[i-2]
     }
     return fibArr;
